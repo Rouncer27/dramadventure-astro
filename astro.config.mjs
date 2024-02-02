c2e38a5f1ff2bcf2f89a0be463158721dc998964
Trevor Rounce <trevor@switchbackcreative.ca>
2024-02-02 11:37:27 -0700

    urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://celebrated-salamander-59336d.netlify.app",
+});
