c196aba5d8715d2235b243e51ad2e1183557d061
Trevor Rounce <trevor@switchbackcreative.ca>
2024-02-14 11:29:21 -0700

    footer work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celebrated-salamander-59336d.netlify.app",
-  integrations: [react()]
+  site: "https://dram.netlify.app",
+  integrations: [react()],
 });
