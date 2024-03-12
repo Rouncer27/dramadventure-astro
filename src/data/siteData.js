86e8ad641762b45e508e8431a6b064e4aa0a2e45
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-12 08:57:07 -0600

    fixed base SEO fields

---
+++ src/data/siteData.js
@@ -0,0 +1,9 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "Dram Adventure",
+  description:
+    "Dram Adventure - But of course adventures run on all sorts of whiskey.",
+  siteImg: `https://dramadventure.com${defaultSrc.src}`,
+  url: "https://dramadventure.com/",
+};
