ec3b1a209b8d7bdd7d5e9c357a5f1af1bfafcde0
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-12 09:42:14 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dramadventure.com",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   image: {
     domains: ["dramadventure.swbdatabases2.ca"],
   },
