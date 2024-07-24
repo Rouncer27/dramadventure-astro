ff6a86dc372b2198217bb83aac0e84da2051d889
Trevor Rounce <trevor@switchbackcreative.ca>
2024-07-24 13:23:04 -0600

    added cookie concent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dramadventure.com",
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   image: {
     domains: ["dramadventure.swbdatabases2.ca"],
   },
